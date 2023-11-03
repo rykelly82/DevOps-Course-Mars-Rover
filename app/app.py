bf57e70db27bb902a2cfc0a544cba5ec4b2ce36f
Ryan Kelly <ryan.kelly@asda.co.uk>
2023-11-03 08:59:47 +0000

    Co-authored-by: mlewiswasdenton <mlewiswasdenton@users.noreply.github.com>

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import requests
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={os.getenv("API_KEY")}&date=2023-03-21')
+    result = response.json()
+    print(result["url"])
+    return render_template('index.html', landing_image=result["url"])
+
+    
+
+
 
 @app.route('/mars')
 def mars():
